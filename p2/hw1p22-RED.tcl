2a54b3aa70a64489cb8866807d51aa918a62f346
Ubuntu <ubuntu@ip-172-31-0-126.us-west-1.compute.internal>
2016-11-05 06:59:14 +0000

    Tren

--- p2/hw1p22-RED.tcl
+++ p2/hw1p22-RED.tcl
@@ -23,7 +23,7 @@ set n8 [$ns node]
 #create links between the nodes
 $ns duplex-link $n1 $n2 10Mb 10ms DropTail
 $ns duplex-link $n2 $n5 10Mb 10ms DropTail
-$ns duplex-link $n2 $n3 1.5Mb 10ms DropTail
+$ns duplex-link $n2 $n3 1.5Mb 10ms RED
 $ns duplex-link $n3 $n4 10Mb 10ms DropTail
 $ns duplex-link $n3 $n6 10Mb 10ms DropTail
 $ns duplex-link $n3 $n8 10Mb 10ms DropTail
